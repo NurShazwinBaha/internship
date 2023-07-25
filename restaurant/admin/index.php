5f20d09c9b40fddf12c05ad11b6464cf305c20ca
NurShazwinBaha <132446054+NurShazwinBaha@users.noreply.github.com>
2023-07-25 09:25:29 +0800

    Add files via upload

---
+++ restaurant/admin/index.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <title>Food Order Website - Home Page</title>
+    </head>
+
+    <body>
+        <!-- Menu Section Starts -->
+        <div class="menu">
+            Menu Goes Here
+        </div>
+        <!-- Menu Section Ends -->
+
+        <!-- Menu Content Section Starts -->
+        <div class="main-content">
+            Main Content Goes Here
+        </div>
+        <!-- Menu Content Section Ends -->
+
+        <!-- Footer Section Starts -->
+        <div class="footer">
+            Footer Goes Here
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
